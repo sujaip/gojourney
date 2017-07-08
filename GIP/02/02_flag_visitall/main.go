0f0e8f2e7a57e77c895b8a3a9651dc0e06e3e1ea
Sujai <sujai.prakasam@in.tesco.com>
2017-07-08 05:13:28 -0400

    flags

--- GIP/02/02_flag_visitall/main.go
+++ GIP/02/02_flag_visitall/main.go
@@ -19,7 +19,8 @@ func main() {
 
 	/* godoc flag VisitAll
 	   flag package has a VisitAll function that accepts a callback function as an argument.
-	   VisitAll iterates over each of the flags executing the callback function on it */
+	   VisitAll iterates over each of the flags executing the callback function on it
+	   We can use VisitAll function to write out our own help text for each flag*/
 	flag.VisitAll(func(flag *flag.Flag) {
 		format := "\t-%s: %s (Default: '%s')\n"
 		fmt.Printf(format, flag.Name, flag.Usage, flag.DefValue)
@@ -30,3 +31,13 @@ func main() {
 		fmt.Printf("Hello %s!\n", *name)
 	}
 }
+
+/*
+https://godoc.org/launchpad.net/gnuflag
+Above gnuflag package has almost exact same API as standard flag package
+With one additional first argument to Parse function of either true or false
+A value of true looks for flags anywhere in the command, and false behaves like standard flag package
+
+https://github.com/jessevdk/go-flags
+Above go-flags package provides Linux and BSD style flags, but uses entirely different API and style
+*/
---
+++ GIP/02/03_go-flags/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+
+	flags "github.com/jessevdk/go-flags"
+)
+
+var opts struct {
+	Name    string `short:"n" long:"name" default:"world" description:"A name to say hello to"`
+	Spanish bool   `short:"s" long:"spanish" description:"Use Spanish Language"`
+}
+
+func main() {
+	flags.Parse(&opts)
+
+	if opts.Spanish == true {
+		fmt.Printf("Hola %s!\n", opts.Name)
+	} else {
+		fmt.Printf("Hello %s!\n", opts.Name)
+	}
+
+}
